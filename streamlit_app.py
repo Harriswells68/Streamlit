98873465b37dd6a655e97f308a77933e27d388cb
Harriswells68 <136472037+Harriswells68@users.noreply.github.com>
2024-06-08 13:46:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,20 @@
 import streamlit as st
-from streamlit_lottie import st_lottie
-import requests
+import streamlit.components.v1 as components
 
-#load lottie animation
-def load_lottieurl(url):
-  r = requests.get(url)
-  if r.status_code != 200:
-    return None
-  return r.json()
+st.set_page_config(layout="wide")
+st.title("Akinator")
 
-lottie_akinaror = load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_mj89j8z4.json")
-
-st.set_page_config(page_title="Akinator", page_icon=":genie:")
-
-st.markdown('<style>body{background-color: #c9e7f1;}</style>', unsafe_allow_html=True)
-st.title(":genie: Akinator")
-
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1, 1])
 
 with col1:
-  st_lottie(lottie_akinaror, speed=1, height=400, key="akinator")
+    components.html("""
+    <img src="https://en.akinator.com/assets/img/akitudes_670x1096/defi.png" style="width:100%; height:auto;">
+    """, height=400)
 
 with col2:
-  st.write("Is your character real?")
-  
-  choices = ["Yes", "No", "Don't know", "Probably", "Probably not"]
+    st.subheader("Is your character real?")
+    options = ['Yes', 'No', 'Don\'t know', 'Probably', 'Probably not']
+    selected_option = st.radio("Select an option:", options)
 
-  choice = st.radio("Select", choices)
-  
-  if st.button("Correct"):
-    st.success("You have guessed correctly!")
+    if st.button("Correct"):
+        st.success("You got it right!")
